41698e2e67f701fc1ee95c7a840a2ca7bcc7046a
Mason <freemason@gmail.com>
2012-11-29 12:10:56 -0800

    some more demo stuff

--- iMancipator/iMancipator/LLViewController.m
+++ iMancipator/iMancipator/LLViewController.m
@@ -58,13 +58,14 @@
         },
     ];
     
-    [self relocate];    
 }
 
 - (void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     if (!self.found) {
-        [self performSelector:@selector(relocate) withObject:self afterDelay:0.5f];
+        [self performSelector:@selector(relocate) withObject:self afterDelay:0.1f];
+    } else {
+        [self relocate];
     }
 }
 
@@ -86,32 +87,29 @@
 
 - (void) relocate {
     MKUserLocation *userLocation = self.map.userLocation;
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance (userLocation.location.coordinate, 1000, 1000);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance (userLocation.location.coordinate, 3000, 3000);
     [self.map setRegion:region animated:!self.found];
     
     if (self.found) {
-        MKPointAnnotation* point;
-        point = [[MKPointAnnotation alloc] init];
-        point.coordinate = CLLocationCoordinate2DMake(userLocation.location.coordinate.latitude + 0.001, userLocation.location.coordinate.longitude + 0.005);
-        [self.map addAnnotation:point];
+        NSLog(@"xx");
+        [data enumerateObjectsUsingBlock:^(NSDictionary* person, NSUInteger idx, BOOL *stop) {
+            MKPointAnnotation* point = [[MKPointAnnotation alloc] init];
+            point.title = [person objectForKey:@"name"];
+            point.subtitle = [person objectForKey:@"activity"];
+            float n = arc4random_uniform(100) % 2 == 0 ? 1 : -1;
+            float m = arc4random_uniform(100) % 3 == 0 ? 0.003f : 0.001f;
+            float o = arc4random_uniform(100) % 5 == 0 ? -0.003f : 0.002f;
+            
+            float rdx1 = (idx * m + o) * n;
+            
+            n = arc4random_uniform(100) % 2 == 0 ? 1 : -1;
+            m = arc4random_uniform(100) % 3 == 0 ? 0.003f : 0.001f;
+            o = arc4random_uniform(100) % 5 == 0 ? -0.003f : 0.002f;
+            float rdx2 = (idx * m + o) * n;
 
-        point = [[MKPointAnnotation alloc] init];
-        point.coordinate = CLLocationCoordinate2DMake(userLocation.location.coordinate.latitude - 0.0001, userLocation.location.coordinate.longitude + 0.001);
-        [self.map addAnnotation:point];
-        
-        point = [[MKPointAnnotation alloc] init];
-        point.coordinate = CLLocationCoordinate2DMake(userLocation.location.coordinate.latitude + 0.001, userLocation.location.coordinate.longitude);
-        [self.map addAnnotation:point];
-        
-        point = [[MKPointAnnotation alloc] init];
-        point.coordinate = CLLocationCoordinate2DMake(userLocation.location.coordinate.latitude - 0.003, userLocation.location.coordinate.longitude - 0.003);
-        [self.map addAnnotation:point];
-        
-        point = [[MKPointAnnotation alloc] init];
-        point.coordinate = CLLocationCoordinate2DMake(userLocation.location.coordinate.latitude + 0.0005, userLocation.location.coordinate.longitude + 0.002);
-        [self.map addAnnotation:point];
-    } else {
-        
+            point.coordinate = CLLocationCoordinate2DMake(userLocation.location.coordinate.latitude - rdx1, userLocation.location.coordinate.longitude + rdx2);
+            [self.map addAnnotation:point];
+        }];
     }
 
 }
@@ -126,14 +124,9 @@
         pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"mapAnnotation"];
     }
     
-//    pin.canShowCallout = YES;
-//    pin.animatesDrop = YES;
-    pin.pinColor = [annotation isKindOfClass:[MKUserLocation class]] ? MKPinAnnotationColorGreen : MKPinAnnotationColorPurple;
-    
-    UILabel* label = [[UILabel alloc] init];
-    label.text = @"hi there";
-    pin.leftCalloutAccessoryView = label;
-    
+    pin.canShowCallout = YES;
+    pin.animatesDrop = YES;
+    pin.pinColor = [annotation isKindOfClass:[MKUserLocation class]] ? MKPinAnnotationColorGreen : MKPinAnnotationColorPurple;    
     
     return pin;
 }
